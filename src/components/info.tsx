7041d005864b24f99dd061c4aec308dffee9b0cf
Mohammed Eshan <itsrandomboy77@gmail.com>
2025-05-25 15:38:23 +0400

    add discord

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -7,17 +7,19 @@ export async function UserInfo() {
 	console.log(session?.user?.name);
 	if (!session?.user) return (
 	<div>
-			<div className="flex flex-col align-left gap-2 p-5 bg-slate-100 rounded-sm">
+			<div className="flex flex-col align-left gap-2 p-5 bg-slate-200 rounded-sm">
 				<p>Not logged in</p>
 			</div>
 	</div>)
 	return (
-		<div className="flex flex-col align-left gap-2 p-5 bg-slate-100 rounded-sm">
+		<div className="flex align-left gap-5 p-5 bg-slate-200 rounded-sm">
 			<Avatar className="w-15 h-15">
 				<AvatarImage src={session?.user?.image} />
 			</Avatar>
-			<h1 className="text-xl">Name: {session.user.name}</h1>
-			<h1 className="text-xl">Email: {session.user.email}</h1>
+			<div className="flex flex-col">
+				<p className="text-2xl font-bold">{session?.user?.name}</p>
+				<p className="text-xl text-gray-500">{session?.user?.email}</p>
+			</div>
 		</div>
 	)
 }
